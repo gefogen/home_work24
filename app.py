23be07fd7c374b8763cf4bd660a16b68618c2fe0
GeFoGen <gefogen>
2022-09-13 12:57:23 +0300

    init

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import os
 
-from marshmallow.exceptions import ValidationError  # type: ignore
 from flask import Flask, request, abort
 
-from project.exceptions import FailRequest
+from project.exceptions import FailRequest, NotValueError, NotTypeError
 from project.schema import Requests, RequestsSchema
 from project.classes import Search
 from settings import DATA_DIR
@@ -25,7 +24,7 @@ def perform_query():  # type: ignore
             cmd1_result = getattr(Search, requests.cmd1)(file, requests.value1)
             cmd2_result = getattr(Search, requests.cmd2)(cmd1_result, requests.value2)
 
-    except (ValueError, FileNotFoundError, TypeError, IndexError, ValidationError) as e:
+    except (NotValueError, FailRequest, NotTypeError) as e:
         abort(400, e)
 
     return app.response_class(cmd2_result, content_type="text/plain")
